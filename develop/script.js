3c9808bd7caafd62f2d0faa93aab6c61e03fe09f
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-04-27 16:18:15 -0500

    not pulling page but workingish

--- develop/script.js
+++ develop/script.js
@@ -1,34 +1,62 @@
-var apiKey = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={68ab3cf80e2e8d6083d2d1e1d2bb8a9a}';
+var apiKey = "68ab3cf80e2e8d6083d2d1e1d2bb8a9a";
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
+var form = document.querySelector("form");
+var cityInput = document.querySelector("#city");
+var cityName = document.querySelector("#city-name");
+var date = document.querySelector("#date");
+var icon = document.querySelector("#icon");
+var temperature = document.querySelector("#temperature");
+var humidity = document.querySelector("#humidity");
+var windSpeed = document.querySelector("#wind-speed");
+var forecastDays = document.querySelectorAll(".forecast-day");
 
-fetch(apiKey + "&q=" + city + "&units=imperial")
-    .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                displayWeather(data, city);
-            });
-        } else {
-            alert("Error: " + response.statusText);
-        }
+// Get weather data for the city
+function getWeatherData(city) {
+  var url = `${apiUrl}?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Update current weather section
+      cityName.textContent = data.name;
+      date.textContent = new Date().toLocaleDateString();
+      icon.innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather Icon">`;
+      temperature.textContent = `${data.main.temp.toFixed(1)}°C`;
+      humidity.textContent = `Humidity: ${data.main.humidity}%`;
+      windSpeed.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+
+      // Update 5-day forecast
+      for (let i = 0; i < 5; i++) {
+        var forecast = data.list[i + 1];
+        var forecastDay = forecastDays[i];
+        forecastDay.querySelector(".date").textContent = new Date(forecast.dt_txt).toLocaleDateString();
+        forecastDay.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/w/${forecast.weather[0].icon}.png" alt="Weather Icon">`;
+        forecastDay.querySelector(".temperature").textContent = `${forecast.main.temp.toFixed(1)}°C`;
+        forecastDay.querySelector(".humidity").textContent = `Humidity: ${forecast.main.humidity}%`;
+        forecastDay.querySelector(".wind-speed").textContent = `Wind Speed: ${forecast.wind.speed} m/s`;
+      }
     })
-    .catch(function (error) {
-        alert("Unable to connect to OpenWeather");
+    .catch(error => {
+      console.log(error);
+      alert("Sorry, could not fetch weather data for the city.");
     });
+}
 
-// var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
-// var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
-
-// var searchForm = document.querySelector('form');
-// var cityNameInput = document.querySelector('#city');
-// var searchBtn = document.querySelector('#search-btn');
-// var cityNameDisplay = document.querySelector('#city-name');
-// var dateDisplay = document.querySelector('#date');
-// var iconDisplay = document.querySelector('#icon');
-// var tempDisplay = document.querySelector('#temperature');
-// var humidityDisplay = document.querySelector('#humidity');
-// var windSpeedDisplay = document.querySelector('#wind-speed');
-// var forecastDisplay = document.querySelector('.forecast');
-// var historyList = document.querySelector('#history-list');
-
-// var history = JSON.parse(localStorage.getItem('history')) || [];
-
+// Handle form submission
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  var city = cityInput.value;
+  if (city) {
+    getWeatherData(city);
+    // Add the city to the search history
+    var historyList = document.querySelector("#history-list");
+    var listItem = document.createElement("li");
+    listItem.textContent = city;
+    historyList.appendChild(listItem);
+    // Clear the input field
+    cityInput.value = "";
+  } else {
+    alert("Please enter a city name.");
+  }
+});
