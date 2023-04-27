1ecee54195a732536de5f2f53d6c27fbac7697a6
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-04-27 16:49:50 -0500

    working but what

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,7 @@
 var apiKey = "68ab3cf80e2e8d6083d2d1e1d2bb8a9a";
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
+
 var form = document.querySelector("form");
 var cityInput = document.querySelector("#city");
 var cityName = document.querySelector("#city-name");
@@ -11,37 +12,40 @@ var humidity = document.querySelector("#humidity");
 var windSpeed = document.querySelector("#wind-speed");
 var forecastDays = document.querySelectorAll(".forecast-day");
 
+
+
+
 // Get weather data for the city
 function getWeatherData(city) {
-  var url = `${apiUrl}?q=${city}&appid=${apiKey}&units=metric`;
+    var url = `${apiUrl}?q=${city}&appid=${apiKey}&units=metric`;
+  
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        // Update current weather section
+        cityName.textContent = data.city.name;
+        date.textContent = new Date().toLocaleDateString();
+        icon.innerHTML = `<img src="http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png" alt="Weather Icon">`;
+        temperature.textContent = `${data.list[0].main.temp.toFixed(1)}°C`;
+        humidity.textContent = `Humidity: ${data.list[0].main.humidity}%`;
+        windSpeed.textContent = `Wind Speed: ${data.list[0].wind.speed} m/s`
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      // Update current weather section
-      cityName.textContent = data.name;
-      date.textContent = new Date().toLocaleDateString();
-      icon.innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather Icon">`;
-      temperature.textContent = `${data.main.temp.toFixed(1)}°C`;
-      humidity.textContent = `Humidity: ${data.main.humidity}%`;
-      windSpeed.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+    
+        // Update 5-day forecast
+        for (var i = 0; i < forecastDays.length; i++) {
+            var forecastIndex = i * 8 + 4;
+            forecastDays[i].querySelector(".forecast-date").textContent = new Date(data.list[forecastIndex].dt_txt).toLocaleDateString();
+            forecastDays[i].querySelector(".forecast-icon").innerHTML = `<img src="http://openweathermap.org/img/w/${data.list[forecastIndex].weather[0].icon}.png" alt="Weather Icon">`;
+            forecastDays[i].querySelector(".forecast-temperature").textContent = `${data.list[forecastIndex].main.temp.toFixed(1)}°C`;
+            forecastDays[i].querySelector(".forecast-humidity").textContent = `Humidity: ${data.list[forecastIndex].main.humidity}%`;
+        }
+        })
+        .catch(error => {
+        alert("There was a problem getting the weather data.");
+        });
+    }
 
-      // Update 5-day forecast
-      for (let i = 0; i < 5; i++) {
-        var forecast = data.list[i + 1];
-        var forecastDay = forecastDays[i];
-        forecastDay.querySelector(".date").textContent = new Date(forecast.dt_txt).toLocaleDateString();
-        forecastDay.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/w/${forecast.weather[0].icon}.png" alt="Weather Icon">`;
-        forecastDay.querySelector(".temperature").textContent = `${forecast.main.temp.toFixed(1)}°C`;
-        forecastDay.querySelector(".humidity").textContent = `Humidity: ${forecast.main.humidity}%`;
-        forecastDay.querySelector(".wind-speed").textContent = `Wind Speed: ${forecast.wind.speed} m/s`;
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      alert("Sorry, could not fetch weather data for the city.");
-    });
-}
+  
 
 // Handle form submission
 form.addEventListener("submit", event => {
